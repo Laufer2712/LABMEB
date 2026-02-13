ac37b775a5e9fccc7c2289c205146da1aaf7ac55
Lau <laurafernandat5@gmail.com>
2026-02-13 16:57:26 -0400

    Ajusticos

---
+++ backend/config.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$dbname = "labmeb";
+$user = "root";
+$pass = "";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
